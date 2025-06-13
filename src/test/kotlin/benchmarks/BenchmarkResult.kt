3c4ab805cc4fd2b9ad369046f0b093fb1db48c9a
sofyak0zyreva <k81sofia@gmail.com>
2025-06-13 18:47:03 +0300

    ref: restructure benchmark result class, benchmarking all modes and sizes into separate files

---
+++ src/test/kotlin/benchmarks/BenchmarkResult.kt
@@ -0,0 +1,22 @@
+package benchmarks
+
+import convolution.ConvolutionMode
+import kotlin.math.sqrt
+
+data class BenchmarkResult(
+    val mode: ConvolutionMode,
+    val times: List<Double>,
+) {
+    private val n: Int get() = times.size
+    private val average: Double get() = times.average()
+    private val stdDev: Double get() = sqrt(times.sumOf { (it - average).let { d -> d * d } } / (n - 1))
+    private val stdErrorOfTheMean: Double get() = stdDev / sqrt(n.toDouble())
+
+    override fun toString(): String {
+        return buildString {
+            appendLine("Mode: $mode")
+            appendLine("  Average Time:          %.4f s".format(average))
+            appendLine("  Std Error Of The Mean: %.4f s".format(stdErrorOfTheMean))
+        }
+    }
+}
---
+++ src/test/kotlin/benchmarks/BenchmarkSizes.kt
@@ -0,0 +1,55 @@
+package benchmarks
+
+import convolution.*
+import createRandomFilter
+import createRandomImage
+import filters.Filter
+import org.bytedeco.opencv.opencv_core.Mat
+
+// function to measure execution time with different batch or tile sizes
+// to then choose the most optimal one to compare across all convolution implementations
+fun benchmarkSizes(
+    image: Mat,
+    filter: Filter,
+    modeFactory: (Int) -> ConvolutionMode,
+    sizesToTry: List<Int>,
+    runs: Int = 10
+): List<BenchmarkResult> {
+    return sizesToTry.map { size ->
+        val mode = modeFactory(size)
+        val times = List(runs) { measureSingleModeTime(image, mode, filter) }
+        BenchmarkResult(mode, times)
+    }
+}
+
+fun main() {
+    val testImage = createRandomImage(512, 512)
+    val testFilter = createRandomFilter(5)
+
+    val testSizes = listOf(
+        1,
+        4,
+        8,
+        16,
+        32,
+        64,
+        128,
+        256,
+        512
+    )
+
+    val benchmarkOptimalSize: ((Int) -> ConvolutionMode) -> List<BenchmarkResult> = { modeFactory ->
+        benchmarkSizes(
+            image = testImage,
+            filter = testFilter,
+            sizesToTry = testSizes,
+            modeFactory = modeFactory
+        )
+    }
+    val rowResult = benchmarkOptimalSize { ConvolutionMode.ParallelRows(it) }
+    val colResult = benchmarkOptimalSize { ConvolutionMode.ParallelCols(it) }
+    val tileResult = benchmarkOptimalSize { size -> ConvolutionMode.ParallelTiles(size, size) }
+    rowResult.forEach(::println)
+    colResult.forEach(::println)
+    tileResult.forEach(::println)
+}
